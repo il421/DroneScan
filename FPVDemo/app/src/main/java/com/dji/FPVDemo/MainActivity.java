3a44d0272f9d07a033d9a67e7f9d4b90ec6019df
Ilya Suglobov <il421@mail.ru>
2018-08-22 17:02:36 +1200

    Removed redunduncy

--- FPVDemo/app/src/main/java/com/dji/FPVDemo/MainActivity.java
+++ FPVDemo/app/src/main/java/com/dji/FPVDemo/MainActivity.java
@@ -62,47 +62,6 @@ public class MainActivity extends Activity implements SurfaceTextureListener,OnC
                 }
             }
         };
-
-        Camera camera = FPVDemoApplication.getCameraInstance();
-
-        if (camera != null) {
-
-            camera.setSystemStateCallback(new SystemState.Callback() {
-                @Override
-                public void onUpdate(SystemState cameraSystemState) {
-                    if (null != cameraSystemState) {
-
-                        int recordTime = cameraSystemState.getCurrentVideoRecordingTimeInSeconds();
-                        int minutes = (recordTime % 3600) / 60;
-                        int seconds = recordTime % 60;
-
-                        final String timeString = String.format("%02d:%02d", minutes, seconds);
-                        final boolean isVideoRecording = cameraSystemState.isRecording();
-
-                        MainActivity.this.runOnUiThread(new Runnable() {
-
-                            @Override
-                            public void run() {
-
-                                recordingTime.setText(timeString);
-
-                                /*
-                                 * Update recordingTime TextView visibility and mRecordBtn's check state
-                                 */
-                                if (isVideoRecording){
-                                    recordingTime.setVisibility(View.VISIBLE);
-                                }else
-                                {
-                                    recordingTime.setVisibility(View.INVISIBLE);
-                                }
-                            }
-                        });
-                    }
-                }
-            });
-
-        }
-
     }
 
     protected void onProductChange() {
@@ -167,33 +126,9 @@ public class MainActivity extends Activity implements SurfaceTextureListener,OnC
         // init mVideoSurface
         mVideoSurface = (TextureView)findViewById(R.id.video_previewer_surface);
 
-        recordingTime = (TextView) findViewById(R.id.timer);
-        mCaptureBtn = (Button) findViewById(R.id.btn_capture);
-        mRecordBtn = (ToggleButton) findViewById(R.id.btn_record);
-        mShootPhotoModeBtn = (Button) findViewById(R.id.btn_shoot_photo_mode);
-        mRecordVideoModeBtn = (Button) findViewById(R.id.btn_record_video_mode);
-
         if (null != mVideoSurface) {
             mVideoSurface.setSurfaceTextureListener(this);
         }
-
-        mCaptureBtn.setOnClickListener(this);
-        mRecordBtn.setOnClickListener(this);
-        mShootPhotoModeBtn.setOnClickListener(this);
-        mRecordVideoModeBtn.setOnClickListener(this);
-
-        recordingTime.setVisibility(View.INVISIBLE);
-
-        mRecordBtn.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    startRecord();
-                } else {
-                    stopRecord();
-                }
-            }
-        });
     }
 
     private void initPreviewer() {
@@ -246,6 +181,8 @@ public class MainActivity extends Activity implements SurfaceTextureListener,OnC
 
     @Override
     public void onSurfaceTextureUpdated(SurfaceTexture surface) {
+        
+
     }
 
     public void showToast(final String msg) {
@@ -259,111 +196,6 @@ public class MainActivity extends Activity implements SurfaceTextureListener,OnC
     @Override
     public void onClick(View v) {
 
-        switch (v.getId()) {
-            case R.id.btn_capture:{
-                captureAction();
-                break;
-            }
-            case R.id.btn_shoot_photo_mode:{
-                switchCameraMode(SettingsDefinitions.CameraMode.SHOOT_PHOTO);
-                break;
-            }
-            case R.id.btn_record_video_mode:{
-                switchCameraMode(SettingsDefinitions.CameraMode.RECORD_VIDEO);
-                break;
-            }
-            default:
-                break;
-        }
-    }
-
-    private void switchCameraMode(SettingsDefinitions.CameraMode cameraMode){
-
-        Camera camera = FPVDemoApplication.getCameraInstance();
-        if (camera != null) {
-            camera.setMode(cameraMode, new CommonCallbacks.CompletionCallback() {
-                @Override
-                public void onResult(DJIError error) {
-
-                    if (error == null) {
-                        showToast("Switch Camera Mode Succeeded");
-                    } else {
-                        showToast(error.getDescription());
-                    }
-                }
-            });
-            }
-    }
-
-    // Method for taking photo
-    private void captureAction(){
-
-        final Camera camera = FPVDemoApplication.getCameraInstance();
-        if (camera != null) {
-
-            SettingsDefinitions.ShootPhotoMode photoMode = SettingsDefinitions.ShootPhotoMode.SINGLE; // Set the camera capture mode as Single mode
-            camera.setShootPhotoMode(photoMode, new CommonCallbacks.CompletionCallback(){
-                    @Override
-                    public void onResult(DJIError djiError) {
-                        if (null == djiError) {
-                            handler.postDelayed(new Runnable() {
-                                @Override
-                                public void run() {
-                                    camera.startShootPhoto(new CommonCallbacks.CompletionCallback() {
-                                        @Override
-                                        public void onResult(DJIError djiError) {
-                                            if (djiError == null) {
-                                                showToast("take photo: success");
-                                            } else {
-                                                showToast(djiError.getDescription());
-                                            }
-                                        }
-                                    });
-                                }
-                            }, 2000);
-                        }
-                    }
-            });
-        }
-    }
-
-    // Method for starting recording
-    private void startRecord(){
-
-        final Camera camera = FPVDemoApplication.getCameraInstance();
-        if (camera != null) {
-            camera.startRecordVideo(new CommonCallbacks.CompletionCallback(){
-                @Override
-                public void onResult(DJIError djiError)
-                {
-                    if (djiError == null) {
-                        showToast("Record video: success");
-                    }else {
-                        showToast(djiError.getDescription());
-                    }
-                }
-            }); // Execute the startRecordVideo API
-        }
-    }
-
-    // Method for stopping recording
-    private void stopRecord(){
-
-        Camera camera = FPVDemoApplication.getCameraInstance();
-        if (camera != null) {
-            camera.stopRecordVideo(new CommonCallbacks.CompletionCallback(){
-
-                @Override
-                public void onResult(DJIError djiError)
-                {
-                    if(djiError == null) {
-                        showToast("Stop recording: success");
-                    }else {
-                        showToast(djiError.getDescription());
-                    }
-                }
-            }); // Execute the stopRecordVideo API
-        }
 
     }
 }
