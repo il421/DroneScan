c39dea36ac8a4d3198c4fbfe3f27bbce5a2b07f2
Ilya Suglobov <il421@mail.ru>
2018-09-28 14:35:41 +1200

    28/09 Add control btns, tips

---
+++ FPVDemo/app/src/main/java/com/dji/FPVDemo/TipDialog.java
@@ -0,0 +1,45 @@
+package com.dji.FPVDemo;
+
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+public class TipDialog extends DialogFragment {
+
+    private static final String TAG = TipDialog.class.getName();
+    public String titleParam;
+    public String textParam;
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.dialog_tip, container, false);
+
+
+        TextView getItClose;
+        TextView dialogTitle;
+        TextView dialogText;
+
+        getItClose = view.findViewById(R.id.dialogClose);
+        dialogTitle = view.findViewById(R.id.dialogTipTitle);
+        dialogTitle.setText(titleParam);
+
+        dialogText = view.findViewById(R.id.dialogTipText);
+        dialogText.setText(textParam);
+
+        getItClose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.e(TAG, "Close dialog");
+                getDialog().dismiss();
+            }
+        });
+
+        return view;
+    }
+}
